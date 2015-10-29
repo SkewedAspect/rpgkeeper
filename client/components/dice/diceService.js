b53781fbc6970642e2a3eb005fb4a8d703aa3482
Christopher S. Case <chris.case@g33xnexus.com>
2015-10-29 10:42:14 -0500

    converted more services to pure ES6.

---
+++ client/components/dice/diceService.js
@@ -0,0 +1,36 @@
+//----------------------------------------------------------------------------------------------------------------------
+/// DiceService
+///
+/// @module
+//----------------------------------------------------------------------------------------------------------------------
+
+import rpgdice from 'rpgdice';
+import LRU from 'lru-cache';
+
+//----------------------------------------------------------------------------------------------------------------------
+
+class DiceService {
+    constructor()
+    {
+        this.cache = LRU(50);
+    } // end constructor
+
+    roll(rollTxt, scope)
+    {
+        // We cache the expression objects, since they're costly to create, and can be evaluated multiple times.
+        var expr = this.cache.get(rollTxt);
+        if(!expr)
+        {
+            expr = rpgdice.parse(rollTxt);
+            this.cache.set(rollTxt, expr);
+        } // end if
+
+        return expr.eval(scope);
+    } // end roll
+} // end DiceService
+
+//----------------------------------------------------------------------------------------------------------------------
+
+export default new DiceService();
+
+//----------------------------------------------------------------------------------------------------------------------
