b5678c58685756cfda46587edc3fbbf921d13f03
Christopher S. Case <chris.case@g33xnexus.com>
2015-10-30 00:57:33 -0500

    actually made character page display the character (as a json object)

--- client/pages/character/character.vue
+++ client/pages/character/character.vue
@@ -1,15 +1,26 @@
 <template>
     <h1>Character goes here!</h1>
-    <code>{{ charID }}</code>
+    <pre>{{ baseChar | json }}</pre>
 </template>
 
 <script type="text/babel">
+    import charSvc from '../../components/character/characterService.js';
+
     export default {
         data: function()
         {
             return {
+                baseChar: null,
+                systemChar: null,
                 charID: this.$route.params.id
             };
+        },
+        ready: function()
+        {
+            charSvc.get(this.charID)
+                .then((char) => {
+                    this.baseChar = char;
+                });
         }
     }
 </script>
