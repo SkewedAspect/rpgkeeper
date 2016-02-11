e107dbb171fc45f00ba89328c8b96d8d3b844bbc
cprieger <christopherrieger@hotmail.com>
2016-02-10 21:54:40 -0600

    Change to remove namespace.

--- server/models.js
+++ server/models.js
@@ -7,54 +7,49 @@
 import trivialModels from 'trivialmodels';
 
 var types = trivialModels.types;
-var db = { errors: trivialModels.errors };
+var db = {errors: trivialModels.errors};
 
 //----------------------------------------------------------------------------------------------------------------------
 
 db.User = trivialModels.define({
-    name: 'User',
+    name: 'user',
     driver: {
         name: 'TrivialDB',
         options: {
-            namespace: {
-                name: 'base',
-                dbPath: 'server/db',
-            },
+            name: 'base',
+            dbPath: 'server/db',
             pk: 'email'
         }
     },
     schema: {
-        email: types.String({ pk: true }),
+        email: types.String({pk: true}),
         name: types.String(),
         admin: types.Boolean({default: false}),
-        created: types.Date({ auto: true }),
+        created: types.Date({auto: true}),
         permissions: types.Object()
     }
 });
 
 db.BaseCharacter = trivialModels.define({
-    name: 'Character',
+    name: 'character',
     driver: {
         name: 'TrivialDB',
         options: {
-            name: 'characters',
-            namespace: {
-                name: 'base',
-                dbPath: 'server/db',
-            },
+            name: 'base',
+            dbPath: 'server/db',
             pk: 'id'
         },
-    schema: {
-        id: types.String({ pk: true }),
-        name: types.String({ required: true }),
-        system: types.String({ required: true }),
-        description: types.String(),
-        portrait: types.String(),
-        thumbnail: types.String(),
-        biography: types.String(),
-        user: types.String({ required: true })
-    }
-});
+        schema: {
+            id: types.String({pk: true}),
+            name: types.String({required: true}),
+            system: types.String({required: true}),
+            description: types.String(),
+            portrait: types.String(),
+            thumbnail: types.String(),
+            biography: types.String(),
+            user: types.String({required: true})
+        }
+    });
 
 //----------------------------------------------------------------------------------------------------------------------
 
