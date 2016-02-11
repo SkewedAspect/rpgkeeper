e62ef93c856fd684dacd267eb953092d07ab10ef
cprieger <christopherrieger@hotmail.com>
2016-02-10 21:01:58 -0600

    Some of the model work done for converting to TrivialDB

--- server/systems/generic/models.js
+++ server/systems/generic/models.js
@@ -5,47 +5,36 @@
 //----------------------------------------------------------------------------------------------------------------------
 
 import _ from 'lodash';
-import connect from 'thinky';
-
-import baseConfig from '../../../config';
 
 //----------------------------------------------------------------------------------------------------------------------
 
-var config = _.assign({}, baseConfig.rethink, { db: 'rpgk_generic' });
+import trivialModels from 'trivialmodels';
 
-var thinky = connect(config);
-var type = thinky.type;
-var r = thinky.r;
+var types = trivialModels.types;
+var db = { errors: trivialModels.errors };
 
-var db = { r, type, errors: thinky.Errors };
 
 //----------------------------------------------------------------------------------------------------------------------
 
-db.Character = thinky.createModel('characters', {
-    id: type.string(),
-    stats: type.array().default([]),
-    counters: type.array().schema(
-        type.object().schema({
-            name: type.string().required(),
-            value: type.number(),
-            min: type.number(),
-            max: type.number(),
-            steps: type.number()
-        }).removeExtra()
-    ).default([]),
-    rolls: type.array().schema(
-        type.object().schema({
-            name: type.string().required(),
-            expression: type.string()
-        }).removeExtra()
-    ).default([]),
-    notes: type.array().schema(
-        type.object().schema({
-            name: type.string().required(),
-            content: type.string()
-        }).removeExtra()
-    ).default([]),
-    user: type.string().required()
+db.Character =  trivialModels.define(
+    {
+        name: 'Character',
+        driver: {
+            name: 'TrivialDB',
+            options: {
+                name: 'characters',
+                dbPath: 'server/db/generic/',
+                pk: 'id'
+            }
+        }
+    },
+     {
+    id: types.String({ pk: true }),
+    stats: types.Array({ default: [] }),
+    counters: types.Array({default: []}),
+    rolls: types.Array({default: []}),
+    notes: types.Array({default: []}),
+    user: types.String({ required: true })
 });
 
 //----------------------------------------------------------------------------------------------------------------------
