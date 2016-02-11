4795f8e43acda6568e2bd7b0c8552ffee9f889fe
cprieger <christopherrieger@hotmail.com>
2016-02-10 21:29:22 -0600

    Should be closer.

--- server/models.js
+++ server/models.js
@@ -11,81 +11,48 @@ var db = { errors: trivialModels.errors };
 
 //----------------------------------------------------------------------------------------------------------------------
 
-db.Account = trivialModels.define({
-    name: 'Account',
+db.User = trivialModels.define({
+    name: 'User',
     driver: {
         name: 'TrivialDB',
         options: {
-            name: 'accounts',
-            dbPath: 'server/db',
+            namespace: {
+                name: 'base',
+                dbPath: 'server/db',
+            },
             pk: 'email'
         }
     },
     schema: {
         email: types.String({ pk: true }),
-        hash: types.String(),
-        salt: types.String({ default: 'super-insecure-salt' }),
-        iterations: types.Number({ default: 10000 }),
-        mutedChats: types.Array({ default: [] }),
-        created: types.Date({ auto: true })
+        name: types.String(),
+        admin: types.Boolean({default: false}),
+        created: types.Date({ auto: true }),
+        permissions: types.Object()
     }
 });
 
-db.Character = trivialModels.define({
+db.BaseCharacter = trivialModels.define({
     name: 'Character',
     driver: {
         name: 'TrivialDB',
         options: {
             name: 'characters',
-            dbPath: 'server/db'
-        }
-    },
+            namespace: {
+                name: 'base',
+                dbPath: 'server/db',
+            },
+            pk: 'id'
+        },
     schema: {
         id: types.String({ pk: true }),
         name: types.String({ required: true }),
-        avatar: types.String({ default: "https://placeholdit.imgix.net/~text?txtsize=33&txt=250%C3%97250&w=250&h=250" }),
-        primary: types.Boolean({ default: false }),
-        account: types.String({ required: true })
-    }
-});
-
-db.Chat = trivialModels.define({
-    name: 'Chat',
-    driver: {
-        name: 'TrivialDB',
-        options: {
-            name: 'chats',
-            dbPath: 'server/db'
-        }
-    },
-    schema: {
-        id: types.String({ pk: true }),
-        name: types.String(),
-        type: types.String({ required: true }),
-        avatar: types.String({ default: "https://placeholdit.imgix.net/~text?txtsize=33&txt=250%C3%97250&w=250&h=250" }),
-        participants: types.Array({ required: true }),
-        characters: types.Array({ default: [] }),
-        owner: types.String(),
-        archived: types.Boolean({ default: false })
-    }
-});
-
-db.Message = trivialModels.define({
-    name: 'Message',
-    driver: {
-        name: 'TrivialDB',
-        options: {
-            name: 'messages',
-            dbPath: 'server/db'
-        }
-    },
-    schema: {
-        id: types.String({ pk: true }),
-        type: types.String({ required: true }),
-        chat: types.String({ required: true }),
-        content: types.String({ required: true }),
-        character: types.String({ required: true }),
-        sent: types.Date({ auto: true })
+        system: types.String({ required: true }),
+        description: types.String(),
+        portrait: types.String(),
+        thumbnail: types.String(),
+        biography: types.String(),
+        user: types.String({ required: true })
     }
 });
 
