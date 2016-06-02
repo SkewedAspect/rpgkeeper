2aa684a84cb5f1bba545b80fa70aa4dade6522d2
Christopher S. Case <chris.case@g33xnexus.com>
2016-06-02 16:15:11 -0500

    Don't repeat common endpoints for systems.

--- systems/generic/system.js
+++ systems/generic/system.js
@@ -4,7 +4,6 @@
 /// @module
 //----------------------------------------------------------------------------------------------------------------------
 
-import _ from 'lodash';
 import express from 'express';
 import logging from 'omega-logger';
 
@@ -30,55 +29,7 @@ router.use(routeUtils.errorLogger(logger));
 
 //----------------------------------------------------------------------------------------------------------------------
 
-router.get('/character/:charID', (req, resp) =>
-{
-    models.Character.get(req.params.charID)
-        .then((character) =>
-        {
-            resp.json(character);
-        })
-        .catch(models.errors.DocumentNotFound, (error) =>
-        {
-            resp.status(404).json({
-                human: "Character not found.",
-                message: error.message,
-                stack: error.stack
-            });
-        });
-});
-
-router.put('/character/:charID', (req, resp) =>
-{
-    if(req.isAuthenticated())
-    {
-        models.Character.get(req.params.charID)
-            .then((character) =>
-            {
-                _.assign(character, req.body);
-                character.$save().then(() => { resp.json(character); });
-            })
-            .catch(models.errors.DocumentNotFound, (error) =>
-            {
-                resp.status(404).json({
-                    human: "Character not found.",
-                    message: error.message,
-                    stack: error.stack
-                });
-            })
-            .catch((error) =>
-            {
-                resp.status(500).json({
-                    human: "Cannot save character.",
-                    message: error.message,
-                    stack: error.stack
-                });
-            });
-    }
-    else
-    {
-        resp.status(403).end();
-    } // end if
-});
+systemMan.buildGeneralEndpoints(router, models);
 
 //----------------------------------------------------------------------------------------------------------------------
 
