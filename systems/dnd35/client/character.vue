ee86d098e930a6fc8485dbfca0eca9c4d811c5e5
Christopher S. Case <chris.case@g33xnexus.com>
2016-06-23 10:28:23 -0500

    rather decent start on the 3.5 sheet.

--- systems/dnd35/client/character.vue
+++ systems/dnd35/client/character.vue
@@ -4,22 +4,100 @@
             <h4 class="text-center">Loading...</h4>
         </div>
         <div v-else>
+            <div style="margin-left: 2px;">
+                <small class="text-muted">
+                    {{ char.fullSystem.name }}
+                </small>
+            </div>
             <header>
                 <div class="name">
-                    <h1>{{ char.name }} <small class="text-muted">{{ char.description }}</small></h1>
+                    <h1>
+                        {{ char.name }}
+                        <!--<small class="text-muted">{{ char.description }}</small>-->
+                        <button class="btn btn-success-outline btn-sm" style="pointer-events: none">
+                            <span v-if="!char.saving">
+                                <i class="fa fa-check"></i>
+                                Saved!
+                            </span>
+                            <span v-else>
+                                <i class="fa fa-spinner fa-spin"></i>
+                                Saving...
+                            </span>
+                        </button>
+                    </h1>
                 </div>
-                <div class="classes">
-                    <button class="close pull-right" title="Edit Classes and Levels" style="margin-left: 10px;">
+                <div class="race">
+                    <h4 class="text-muted">
+                        {{ char.race.name }}
+                    </h4>
+                    <button class="close" title="Edit Race">
                         <i class="fa fa-edit"></i>
                     </button>
+                </div>
+                <div class="classes">
                     <h4 class="text-muted">
                         <span v-for="classObj in char.classes">
-                            {{ classObj.level }} {{ classObj.class.name }}
+                            {{ classObj.class.name }} {{ classObj.level }}
                             <span v-if="$index != (char.classes.length - 1)">/</span>
                         </span>
                     </h4>
+                    <button class="close" title="Edit Classes and Levels">
+                        <i class="fa fa-edit"></i>
+                    </button>
                 </div>
             </header>
+
+            <div class="upper-sheet">
+                <div class="portrait">
+                    <img class="img-thumbnail"
+                         :src="char.portrait || 'http://placehold.it/800x1200'"
+                         height="445px"
+                         width="300px"
+                         style="max-width: inherit">
+                </div>
+                <div class="bio card">
+                    <div class="card-header">
+                        <i class="fa fa-list"></i>
+                        Details
+                    </div>
+                    <div class="card-block flex-form">
+                        <div class="form-group age">
+                            <input id="age" type="number" class="form-control" v-model="char.age" debounce="500" number>
+                            <label for="age">Age</label>
+                        </div>
+                        <div class="form-group height">
+                            <input id="height" type="text" class="form-control" v-model="char.height" debounce="500">
+                            <label for="height">Height</label>
+                        </div>
+                        <div class="form-group size">
+                            <p class="form-control-static">{{ char.size }}</p>
+                            <label>Size</label>
+                        </div>
+                        <div class="form-group gender">
+                            <select id="gender" class="form-control" v-model="char.gender">
+                                <option :value="undefined" :selected="!char.gender">Please select...</option>
+                                <option :value="opt.value" v-for="opt in char.genderOptions">
+                                    {{ opt.display }}
+                                </option>
+                            </select>
+                            <label for="gender">Gender</label>
+                        </div>
+                        <div class="form-group alignment">
+                            <select id="alignment" class="form-control" v-model="char.alignment">
+                                <option :value="undefined" :selected="!char.alignment">Please select...</option>
+                                <option :value="opt.value" v-for="opt in char.alignmentOptions">
+                                    {{ opt.display }}
+                                </option>
+                            </select>
+                            <label for="alignment">Alignment</label>
+                        </div>
+                        <div class="form-group speed">
+                            <input id="speed" type="number" class="form-control" v-model="char.speed" debounce="500" number>
+                            <label for="speed">Speed</label>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
             <!-- Notes -->
             <notes :notes="char.notes" :save="save"></notes>
@@ -30,19 +108,104 @@
 <style rel="stylesheet/scss" lang="sass">
     #dnd35-char {
         header {
-            display: table;
-            width: 100%;
+            display: flex;
 
             .name {
-                display: table-cell;
-                vertical-align: bottom;
+                flex: 1 1 auto;
             }
 
+            .race {
+                margin-right: 10px;
+            }
+
+            .race,
             .classes {
-                display: table-cell;
-                vertical-align: bottom;
-                text-align: right;
-                padding-bottom: 3px;
+                flex: 0 0 auto;
+                line-height: 3.5rem;
+
+                h4 {
+                    display: inline-block;
+                }
+
+                button.close {
+                    margin-top: 1.25rem;
+                    display: inline-block;
+                    margin-left: 5px;
+                    font-size: 1em;
+                }
+
+            }
+        }
+
+        .upper-sheet {
+            display: flex;
+            flex-wrap: wrap;
+
+            .portrait {
+                flex: 0 0 300px;
+                margin-right: 5px;
+                margin-bottom: 5px;
+
+                img {
+                    height: 445px;
+                    width: 300px;
+                }
+            }
+
+            .bio {
+                flex: 2 2 300px;
+                margin-bottom: 5px;
+                min-width: 300px;
+
+                .age {
+                    flex-basis: 60px;
+                    min-width: 60px;
+                }
+
+                .height {
+                    flex-basis: 60px;
+                    min-width: 60px;
+                }
+
+                .size {
+                    flex-basis: 80px;
+                    min-width: 80px;
+                }
+
+                .gender {
+                    flex-basis: 100px;
+                    min-width: 100px;
+                }
+
+                .alignment {
+                    flex-basis: 120px;
+                    min-width: 120px;
+                }
+
+                .speed {
+                    flex-basis: 50px;
+                    min-width: 50px;
+                }
+            }
+        }
+
+        .flex-form {
+            display: flex;
+            flex-wrap: wrap;
+            margin-left: -2.5px;
+            margin-right: -2.5px;
+
+            .form-group {
+                margin-left: 2.5px;
+                margin-right: 2.5px;
+                flex: 1 1 auto;
+
+                label {
+                    display: block;
+                    color: #777;
+                    font-size: .85rem;
+                    text-align: center;
+                }
             }
         }
     }
@@ -72,6 +235,7 @@
         methods: {
             save()
             {
+                console.log('save?');
                 this.char.save();
             }
         },
