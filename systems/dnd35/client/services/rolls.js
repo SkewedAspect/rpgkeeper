932788450706472e0e20608ea7f243593c010ce6
Christopher S. Case <chris.case@g33xnexus.com>
2016-06-27 14:47:31 -0500

    implemented rolls component.

---
+++ systems/dnd35/client/services/rolls.js
@@ -0,0 +1,43 @@
+//----------------------------------------------------------------------------------------------------------------------
+/// RollsService
+///
+/// @module
+//----------------------------------------------------------------------------------------------------------------------
+
+import Vue from 'vue';
+import diceSvc from '../../../../client/components/dice/diceService';
+
+//----------------------------------------------------------------------------------------------------------------------
+
+class RollsService {
+    constructor()
+    {
+        this.state = {
+            results: []
+        };
+    } // end constructor
+    
+    clearResults()
+    {
+        Vue.set(this.state, 'results', []);
+    } // end clearResults
+    
+    roll(rollText, scope, title)
+    {
+        var roll = diceSvc.roll(rollText, scope);
+        
+        this.state.results.unshift({
+            title,
+            roll: {
+                value: roll.value,
+                text: roll.render()
+            }
+        });
+    } // end roll
+} // end RollsService
+
+//----------------------------------------------------------------------------------------------------------------------
+
+export default new RollsService();
+
+//----------------------------------------------------------------------------------------------------------------------
