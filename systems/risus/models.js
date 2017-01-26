844e1a735fb636f285ed66180a07ef97623753a7
Christopher S. Case <chris.case@g33xnexus.com>
2017-01-26 15:06:14 -0600

    initial risus implementation.

---
+++ systems/risus/models.js
@@ -0,0 +1,40 @@
+//----------------------------------------------------------------------------------------------------------------------
+// Database models for Edge of the Empire System
+//
+// @module models
+//----------------------------------------------------------------------------------------------------------------------
+
+const connect = require('thinky');
+const config = require('../../config');
+
+//----------------------------------------------------------------------------------------------------------------------
+
+const thinky = connect(config.rethink);
+const type = thinky.type;
+const r = thinky.r;
+
+const db = { r, type, errors: thinky.Errors };
+
+//----------------------------------------------------------------------------------------------------------------------
+
+db.Character = thinky.createModel('risus_characters', {
+    id: type.string(),
+    cliches: type.array().schema({
+        value: type.number().integer().min(1).default(1),
+        description: type.string().required(),
+        tools: type.string()
+    }).default([]),
+    hooks: type.array().schema({
+            description: type.string().required()
+        }).default([]),
+    luckyShots: type.object().schema({
+        current: type.number().integer().min(0).default(0),
+        max: type.number().integer().min(0).default(0),
+    }).default({})
+}, { enforce_extra: "remove" });
+
+//----------------------------------------------------------------------------------------------------------------------
+
+module.exports = db;
+
+//----------------------------------------------------------------------------------------------------------------------
