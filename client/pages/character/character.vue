cdf9b25535ec2dad97e0ac7c85a0b3afd36a40fd
Christopher S. Case <chris.case@g33xnexus.com>
2017-01-26 18:16:39 -0600

    more error handling, as well as cleanup.

--- client/pages/character/character.vue
+++ client/pages/character/character.vue
@@ -4,9 +4,24 @@
 
 <template>
     <div id="character-page">
-        <component v-if="loaded" :is="character.system.id" :character="character"></component>
-        <div class="loading container text-center"v-else>
+        <component v-if="!error && loaded" :is="character.system.id" :character="character"></component>
+        <div class="loading container text-center"v-else-if="!loaded">
             <loader></loader>
+        </div>
+        <div v-if="error" class="container" style="margin: 16px">
+            <md-card class="md-warn" style="flex: 1">
+                <md-card-header>
+                    <div class="md-title">Error loading character</div>
+                </md-card-header>
+
+                <md-card-content>
+                    {{ error.stack }}
+                </md-card-content>
+
+                <md-card-actions>
+                    <md-button @click="goTo('/dashboard')">Go to Dashboard</md-button>
+                </md-card-actions>
+            </md-card>
         </div>
     </div>
 </template>
@@ -30,7 +45,9 @@
     import spinkit from '../../components/spinkit';
 
     // Systems
+//    import GenericCharacter from '../../../systems/generic/client/character.vue';
     import RisusCharacter from '../../../systems/risus/client/character.vue';
+    import EoteCharacter from '../../../systems/eote/client/character.vue';
 
     //------------------------------------------------------------------------------------------------------------------
 
@@ -40,14 +57,23 @@
             loader: spinkit.wave,
 
             // Systems
-            risus: RisusCharacter
+//            generic: GenericCharacter,
+            risus: RisusCharacter,
+            eote: EoteCharacter
         },
         data()
         {
             return {
                 loaded: false,
+                error: undefined,
                 character: undefined
             };
+        },
+        methods: {
+            goTo(path)
+            {
+                this.$router.push(path);
+            }
         },
         mounted()
         {
@@ -57,6 +83,11 @@
                     this.character = character;
                     return character.populateSystem()
                         .then(() => { this.loaded = true; });
+                })
+                .catch((error) =>
+                {
+                    this.loaded = true;
+                    this.error = error;
                 });
         }
     }
