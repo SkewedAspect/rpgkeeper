a8b735166e8b770871a734d53485d8211cafd8a5
Christopher S. Case <chris.case@g33xnexus.com>
2017-09-23 23:05:24 -0500

    finished edit modal.

--- systems/fate/client/consequences.vue
+++ systems/fate/client/consequences.vue
@@ -15,9 +15,9 @@
                     <tr>
                         <td><b>2</b></td>
                         <td>
-                            <span v-if="mildConsequences[0]">
-                                {{ mildConsequences[0].detail }}
-                                <small v-if="mildConsequences[0].healing"><i>(Healing)</i></small>
+                            <span v-if="mildConsequence1.detail">
+                                {{ mildConsequence1.detail }}
+                                <small v-if="mildConsequence1.healing"><i>(Healing)</i></small>
                             </span>
                             <span class="placeholder" v-else>Mild</span>
                         </td>
@@ -25,7 +25,7 @@
                     <tr>
                         <td><b>4</b></td>
                         <td>
-                            <span v-if="moderateConsequence">
+                            <span v-if="moderateConsequence.detail">
                                 {{ moderateConsequence.detail }}
                                 <small v-if="moderateConsequence.healing"><i>(Healing)</i></small>
                             </span>
@@ -35,7 +35,7 @@
                     <tr>
                         <td><b>6</b></td>
                         <td>
-                            <span v-if="severeConsequence">
+                            <span v-if="severeConsequence.detail">
                                 {{ severeConsequence.detail }}
                                 <small v-if="severeConsequence.healing"><i>(Healing)</i></small>
                             </span>
@@ -51,11 +51,11 @@
                             <b>2</b>
                         </td>
                         <td>
-                            <span v-if="mildConsequences[1] && extraMildType !== 'none'">
-                                {{ mildConsequences[1].detail }}
-                                <small v-if="mildConsequences[1].healing"><i>(Healing)</i></small>
+                            <span v-if="mildConsequence2.detail && extraMildType !== 'none'">
+                                {{ mildConsequence2.detail }}
+                                <small v-if="mildConsequence2.healing"><i>(Healing)</i></small>
                             </span>
-                            <span class="placeholder">
+                            <span class="placeholder" v-else>
                                 Mild
                                 <span v-if="extraMildType !== 'none'">({{ extraMildType }})</span>
                             </span>
@@ -67,9 +67,55 @@
 
         <!-- Edit Dialog -->
         <md-dialog ref="editDialog">
-            <md-dialog-title>Edit Dialogs</md-dialog-title>
+            <md-dialog-title>Edit Consequences</md-dialog-title>
             <md-dialog-content>
-                Nemo, nobis necessitatibus ut illo, ducimus ex.
+                <md-layout>
+                    <md-layout md-flex="75">
+                        <md-input-container md-clearable>
+                            <label>2: Mild</label>
+                            <md-input v-model="mildConsequence1Edit.detail"></md-input>
+                        </md-input-container>
+                    </md-layout>
+                    <md-layout md-flex="25" style="padding-right: 50px;">
+                        <md-checkbox v-model="mildConsequence1Edit.healing" :disabled="!mildConsequence1Edit.detail">Healing</md-checkbox>
+                    </md-layout>
+                </md-layout>
+                <md-layout>
+                    <md-layout md-flex="75">
+                        <md-input-container md-clearable>
+                            <label>
+                                2: Mild
+                                <span v-if="extraMildType !== 'none'">({{ extraMildType }})</span>
+                            </label>
+                            <md-input v-model="mildConsequence2Edit.detail" :disabled="extraMildType === 'none'"></md-input>
+                        </md-input-container>
+                    </md-layout>
+                    <md-layout md-flex="25" style="padding-right: 50px;">
+                        <md-checkbox v-model="mildConsequence2Edit.healing" :disabled="!mildConsequence2Edit.detail || extraMildType === 'none'">Healing</md-checkbox>
+                    </md-layout>
+                </md-layout>
+                <md-layout>
+                    <md-layout md-flex="75">
+                        <md-input-container md-clearable>
+                            <label>4: Moderate</label>
+                            <md-input v-model="moderateConsequenceEdit.detail"></md-input>
+                        </md-input-container>
+                    </md-layout>
+                    <md-layout md-flex="25" style="padding-right: 50px;">
+                        <md-checkbox v-model="moderateConsequenceEdit.healing" :disabled="!moderateConsequenceEdit.detail">Healing</md-checkbox>
+                    </md-layout>
+                </md-layout>
+                <md-layout>
+                    <md-layout md-flex="75">
+                        <md-input-container md-clearable>
+                            <label>6: Severe</label>
+                            <md-input v-model="severeConsequenceEdit.detail"></md-input>
+                        </md-input-container>
+                    </md-layout>
+                    <md-layout md-flex="25" style="padding-right: 50px;">
+                        <md-checkbox v-model="severeConsequenceEdit.healing" :disabled="!severeConsequenceEdit.detail">Healing</md-checkbox>
+                    </md-layout>
+                </md-layout>
             </md-dialog-content>
 
             <md-dialog-actions>
@@ -93,11 +139,15 @@
                 margin-bottom: 0;
 
                 &:first-child {
-                    padding-right: 8px;
+                    @media(min-width: 600px) {
+                        padding-right: 8px;
+                    }
                 }
 
                 &:last-child {
-                    padding-left: 8px;
+                    @media(min-width: 600px) {
+                        padding-left: 8px;
+                    }
                 }
 
                 & > .table-left {
@@ -107,7 +157,9 @@
                     }
 
                     tr td:last-child {
-                        border-right-width: 1px;
+                        @media(min-width: 600px) {
+                            border-right-width: 1px;
+                        }
                     }
                 }
 
@@ -126,14 +178,25 @@
 
                         width: 24px;
                         text-align: center;
-                        border-left-width: 1px;
-                        border-right-width: 0;
+
+                        @media(min-width: 600px) {
+                            border-left-width: 1px;
+                            border-right-width: 0;
+                        }
                     }
                 }
 
                 & > .md-static-table {
                     tr:last-child td {
                         border-bottom-width: 1px;
+                    }
+                }
+
+                & > .table-right {
+                    tr:last-child td {
+                        @media(max-width: 599px) {
+                            border-bottom-width: 0;
+                        }
                     }
                 }
             }
@@ -147,6 +210,9 @@
     //------------------------------------------------------------------------------------------------------------------
     
     import _ from 'lodash';
+
+    // Pull in the shortID utility
+    import { shortID } from '../../../server/utilities';
     
     //------------------------------------------------------------------------------------------------------------------
 
@@ -159,9 +225,10 @@
         },
         computed: {
             aspects(){ return this.character.aspects; },
-            mildConsequences(){ return _.filter(this.aspects, { type: 'consequence', value: 2 }); },
-            moderateConsequence(){ return _.filter(this.aspects, { type: 'consequence', value: 4 })[0]; },
-            severeConsequence(){ return _.filter(this.aspects, { type: 'consequence', value: 6 })[0]; },
+            mildConsequence1(){ return _.filter(this.aspects, { type: 'consequence', value: 2 })[0] || { detail: "", healing: false, value: 2 }; },
+            mildConsequence2(){ return _.filter(this.aspects, { type: 'consequence', value: 2 })[1] || { detail: "", healing: false, value: 2 }; },
+            moderateConsequence(){ return _.filter(this.aspects, { type: 'consequence', value: 4 })[0] || { detail: "", healing: false, value: 4 }; },
+            severeConsequence(){ return _.filter(this.aspects, { type: 'consequence', value: 6 })[0] || { detail: "", healing: false, value: 6 }; },
             extraMildType()
             {
                 const will = _.find(this.character.skills, { name: "Will" });
@@ -186,14 +253,96 @@
             }
         },
         methods: {
+            addOrUpdateAspect(aspect)
+            {
+                if(aspect.id)
+                {
+                    const originalAspect = _.find(this.aspects, { id: aspect.id });
+                    originalAspect.detail = aspect.detail;
+                    originalAspect.healing = !!aspect.healing;
+                }
+                else
+                {
+                    this.aspects.push({ id: shortID(), detail: aspect.detail, healing: aspect.healing, value: aspect.value, type: "consequence" });
+                } // end if
+            },
+            removeAspect(aspect)
+            {
+                const aspectIndex = _.findIndex(this.aspects, { id: aspect.id });
+                if(aspectIndex)
+                {
+                    this.aspects.splice(aspectIndex, 1);
+                } // end if
+            },
             openEdit()
             {
+                // Reset the edit fields
+                this.mildConsequence1Edit = _.cloneDeep(this.mildConsequence1);
+                this.mildConsequence2Edit = _.cloneDeep(this.mildConsequence2);
+                this.moderateConsequenceEdit = _.cloneDeep(this.moderateConsequence);
+                this.severeConsequenceEdit = _.cloneDeep(this.severeConsequence);
+
                 this.$refs.editDialog.open();
             },
             closeEdit(save)
             {
+                if(save)
+                {
+                    if(this.mildConsequence1Edit.detail)
+                    {
+                        this.addOrUpdateAspect(this.mildConsequence1Edit);
+                    }
+                    else if(this.mildConsequence1Edit.id)
+                    {
+                        this.removeAspect(this.mildConsequence1Edit)
+                    } // end if
+
+                    if(this.mildConsequence2Edit.detail)
+                    {
+                        this.addOrUpdateAspect(this.mildConsequence2Edit);
+                    }
+                    else if(this.mildConsequence2Edit.id)
+                    {
+                        this.removeAspect(this.mildConsequence2Edit)
+                    } // end if
+
+                    if(this.moderateConsequenceEdit.detail)
+                    {
+                        this.addOrUpdateAspect(this.moderateConsequenceEdit);
+                    }
+                    else if(this.moderateConsequenceEdit.id)
+                    {
+                        this.removeAspect(this.moderateConsequenceEdit)
+                    } // end if
+
+                    if(this.severeConsequenceEdit.detail)
+                    {
+                        this.addOrUpdateAspect(this.severeConsequenceEdit);
+                    }
+                    else if(this.severeConsequenceEdit.id)
+                    {
+                        this.removeAspect(this.severeConsequenceEdit)
+                    } // end if
+                } // end if
+
+                // Reset the edit fields
+                this.mildConsequence1Edit = _.cloneDeep(this.mildConsequence1);
+                this.mildConsequence2Edit = _.cloneDeep(this.mildConsequence2);
+                this.moderateConsequenceEdit = _.cloneDeep(this.moderateConsequence);
+                this.severeConsequenceEdit = _.cloneDeep(this.severeConsequence);
+
+                // Close the dialog
                 this.$refs.editDialog.close();
             }
+        },
+        data()
+        {
+            return {
+                mildConsequence1Edit: { detail: "", healing: false, value: 2 },
+                mildConsequence2Edit: { detail: "", healing: false, value: 2 },
+                moderateConsequenceEdit: { detail: "", healing: false, value: 4 },
+                severeConsequenceEdit: { detail: "", healing: false, value: 6 }
+            };
         }
     }
 </script>
