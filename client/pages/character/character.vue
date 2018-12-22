a41158bb970809273047e1fca0e92c666730a821
Christopher S. Case <chris.case@g33xnexus.com>
2018-12-21 20:32:25 -0600

    switched to rxjs, converted characters.

--- client/pages/character/character.vue
+++ client/pages/character/character.vue
@@ -4,7 +4,15 @@
 
 <template>
     <div id="character-page">
-        <component v-if="!error && loaded" :is="character.system.id" :character="character"></component>
+        <md-tabs v-if="!error && baseChar" class="md-transparent" md-right>
+            <!-- TODO: Add support for dynamic tabs. -->
+            <md-tab md-label="Sheet">
+                <component :is="baseChar.system"></component>
+            </md-tab>
+            <md-tab md-label="Notes">
+                <notes :disabled="!isAuthorized"></notes>
+            </md-tab>
+        </md-tabs>
         <div class="loading container text-center"v-else-if="!loaded">
             <h4 class="text-center">Loading...</h4>
             <md-progress class="md-accent" md-indeterminate></md-progress>
@@ -43,10 +51,17 @@
 <script>
     //------------------------------------------------------------------------------------------------------------------
 
-    import charSvc from '../../services/character';
+    import _ from 'lodash';
+
+    // Managers
+    import authMan from '../../api/managers/auth';
+    import charMan from '../../api/managers/character';
+    import sysCharMan from '../../api/managers/sysCharacter';
+
+    // Components
+    import NotesComponent from '../../components/notes/notes.vue';
 
     // Systems
-//    import GenericCharacter from '../../../systems/generic/client/character.vue';
     import RisusCharacter from '../../../systems/risus/client/character.vue';
     import FateCharacter from '../../../systems/fate/client/character.vue';
     import EoteCharacter from '../../../systems/eote/client/character.vue';
@@ -56,41 +71,56 @@
     export default {
         name: "character-page",
         components: {
+            notes: NotesComponent,
 
             // Systems
-//            generic: GenericCharacter,
             fate: FateCharacter,
             risus: RisusCharacter,
             eote: EoteCharacter
         },
+        subscriptions: {
+            account: authMan.account$,
+            baseChar: charMan.selected$,
+            sysChar: sysCharMan.selected$
+        },
+        computed: {
+            isAuthorized(){ return _.get(this.account, 'email', 'nope!') === this.baseChar.owner; }
+        },
+        methods: {
+            goTo(path){ this.$router.push(path); }
+        },
         data()
         {
             return {
-                loaded: false,
-                error: undefined,
-                character: undefined
+                error: undefined
             };
         },
-        methods: {
-            goTo(path)
+        created()
+        {
+            // Watch for changes to the baseChar, and save.
+            this.$watch('baseChar', (char, oldChar) =>
             {
-                this.$router.push(path);
-            }
+                if(!_.isUndefined(oldChar) && char)
+                {
+                    charMan.save(char);
+                } // end char
+            }, { deep: true });
+
+            // Watch for changes to the sysChar, and save.
+            this.$watch('sysChar', (char, oldChar) =>
+            {
+                if(!_.isUndefined(oldChar) && char)
+                {
+                    sysCharMan.save(char);
+                } // end char
+            }, { deep: true });
+
         },
         mounted()
         {
-            this.loading = charSvc.get(this.$route.params.id)
-                .then((character) =>
-                {
-                    this.character = character;
-                    return character.populateSystem()
-                        .then(() => { this.loaded = true; });
-                })
-                .catch((error) =>
-                {
-                    this.loaded = true;
-                    this.error = error;
-                });
+            // We always select the character that matches our route, so we handle navigation.
+            charMan.select(this.$route.params.id)
+                .catch((e) => this.error = e);
         }
     }
 </script>
--- systems/risus/client/character.vue
+++ systems/risus/client/character.vue
@@ -3,107 +3,104 @@
 <!--------------------------------------------------------------------------------------------------------------------->
 
 <template>
-    <div id="risus-character" class="container">
-        <character :character="character">
-
-            <!-- Sheet -->
-            <md-layout md-gutter="16">
-                <portrait :src="character.portrait"></portrait>
-                <md-layout md-flex-xsmall="100" style="min-width: 275px">
-                    <md-card style="flex: 1">
-                        <md-toolbar class="md-dense">
-                            <h2 class="md-title">Bio</h2>
-                        </md-toolbar>
-                        <md-card-content style="flex: 1">
-                            <md-input-container>
-                                <label>Name</label>
-                                <md-input v-model="character.name" :disabled="!isAuthorized"></md-input>
-                            </md-input-container>
-                            <md-input-container>
-                                <label>Description</label>
-                                <md-textarea v-model="character.biography" :disabled="!isAuthorized"></md-textarea>
-                            </md-input-container>
-                            <md-layout md-gutter="16">
-                                <md-layout md-flex="50">
-                                    <md-input-container>
-                                        <label class="hidden-sm-up">Adv. Points</label>
-                                        <label class="hidden-xs-down">Advancement Points</label>
-                                        <md-input type="number" v-model.number="advPoints" :disabled="!isAuthorized" min="0" @blur.native="onAdvBlur()"></md-input>
-                                    </md-input-container>
-                                </md-layout>
-                                <md-layout md-flex="50">
-                                    <md-input-container>
-                                        <label class="hidden-sm-up">F & F Dice</label>
-                                        <label class="hidden-xs-down">Fire and Forget Dice</label>
-                                        <md-input type="number" v-model.number="ffDice" :disabled="!isAuthorized" min="0" @blur.native="onFFBlur()"></md-input>
-                                    </md-input-container>
-                                </md-layout>
+    <div id="risus-character" class="container" v-if="character">
+        <!-- Sheet -->
+        <md-layout md-gutter="16">
+            <portrait :src="baseChar.portrait"></portrait>
+            <md-layout md-flex-xsmall="100" style="min-width: 275px">
+                <md-card style="flex: 1">
+                    <md-toolbar class="md-dense">
+                        <h2 class="md-title">Bio</h2>
+                    </md-toolbar>
+                    <md-card-content style="flex: 1">
+                        <md-input-container>
+                            <label>Name</label>
+                            <md-input v-model="baseChar.name" :disabled="!isAuthorized"></md-input>
+                        </md-input-container>
+                        <md-input-container>
+                            <label>Description</label>
+                            <md-textarea v-model="baseChar.biography" :disabled="!isAuthorized"></md-textarea>
+                        </md-input-container>
+                        <md-layout md-gutter="16">
+                            <md-layout md-flex="50">
+                                <md-input-container>
+                                    <label class="hidden-sm-up">Adv. Points</label>
+                                    <label class="hidden-xs-down">Advancement Points</label>
+                                    <md-input type="number" v-model.number="advPoints" :disabled="!isAuthorized" min="0" @blur.native="onAdvBlur()"></md-input>
+                                </md-input-container>
                             </md-layout>
-                            <pool name="Lucky Shots" :pool="character.luckyShots" :edit-disabled="!isAuthorized"></pool>
-                        </md-card-content>
-                    </md-card>
-                </md-layout>
-                <md-layout md-flex-xsmall="100" id="rolls">
-                    <md-card style="flex: 1">
-                        <md-toolbar class="md-dense">
-                            <h2 class="md-title">Rolls</h2>
-                        </md-toolbar>
-                        <md-card-content style="flex: 1; padding-bottom: 0">
-                            <md-input-container style="margin-bottom: 10px;" :disabled="!isAuthorized">
-                                <label>Dice</label>
-                                <md-input type="number" min="0" v-model="dice"></md-input>
-                                <span style="margin-left: 10px; padding-top: 4px">D6</span>
-                            </md-input-container>
-                            <md-list class="md-double-line md-dense roll-list">
-                                <md-list-item v-for="item in rolls">
-                                    <div class="md-list-text-container">
-                                        <span>{{ item.display }}</span>
-                                        <span>{{ item.name }}</span>
-                                    </div>
-                                </md-list-item>
-                            </md-list>
-                        </md-card-content>
-                        <md-card-actions>
-                            <md-button @click.native="roll()" :disabled="!isAuthorized">Roll</md-button>
-                            <md-button @click.native="clearRolls()" :disabled="!isAuthorized">Clear</md-button>
-                        </md-card-actions>
-                    </md-card>
-                </md-layout>
-                <md-layout md-flex-small="100" style="min-width: 50%">
-                    <md-card style="flex: 1">
-                        <md-toolbar class="md-dense">
-                            <h2 class="md-title">Clichés</h2>
-                        </md-toolbar>
-                        <md-card-content style="flex: 1">
-                            <md-list class="md-double-line">
-                                <cliche-item  v-for="(cliche, index) in cliches" :cliche="cliche"
-                                              @click.native="rollCliche(cliche)"
-                                              @deleted="onDeleteCliche(index)" :disabled="!isAuthorized"></cliche-item>
-                            </md-list>
-                        </md-card-content>
-                        <md-card-actions>
-                            <md-button @click.native="openNewCliche()" :disabled="!isAuthorized">Add Cliche</md-button>
-                        </md-card-actions>
-                    </md-card>
-                </md-layout>
-                <md-layout md-flex-small="100" style="min-width: 50%">
-                    <md-card style="flex: 1">
-                        <md-toolbar class="md-dense">
-                            <h2 class="md-title">Hooks</h2>
-                        </md-toolbar>
-                        <md-card-content style="flex: 1">
-                            <md-list>
-                                <hook-item  v-for="(hook, index) in hooks" :hook="hook"
-                                            @deleted="onDeleteHook(index)" :disabled="!isAuthorized"></hook-item>
-                            </md-list>
-                        </md-card-content>
-                        <md-card-actions>
-                            <md-button @click.native="openNewHook()" :disabled="!isAuthorized">Add Hook</md-button>
-                        </md-card-actions>
-                    </md-card>
-                </md-layout>
+                            <md-layout md-flex="50">
+                                <md-input-container>
+                                    <label class="hidden-sm-up">F & F Dice</label>
+                                    <label class="hidden-xs-down">Fire and Forget Dice</label>
+                                    <md-input type="number" v-model.number="ffDice" :disabled="!isAuthorized" min="0" @blur.native="onFFBlur()"></md-input>
+                                </md-input-container>
+                            </md-layout>
+                        </md-layout>
+                        <pool name="Lucky Shots" :pool="character.luckyShots" :edit-disabled="!isAuthorized"></pool>
+                    </md-card-content>
+                </md-card>
             </md-layout>
-        </character>
+            <md-layout md-flex-xsmall="100" id="rolls">
+                <md-card style="flex: 1">
+                    <md-toolbar class="md-dense">
+                        <h2 class="md-title">Rolls</h2>
+                    </md-toolbar>
+                    <md-card-content style="flex: 1; padding-bottom: 0">
+                        <md-input-container style="margin-bottom: 10px;" :disabled="!isAuthorized">
+                            <label>Dice</label>
+                            <md-input type="number" min="0" v-model="dice"></md-input>
+                            <span style="margin-left: 10px; padding-top: 4px">D6</span>
+                        </md-input-container>
+                        <md-list class="md-double-line md-dense roll-list">
+                            <md-list-item v-for="item in rolls">
+                                <div class="md-list-text-container">
+                                    <span>{{ item.display }}</span>
+                                    <span>{{ item.name }}</span>
+                                </div>
+                            </md-list-item>
+                        </md-list>
+                    </md-card-content>
+                    <md-card-actions>
+                        <md-button @click.native="roll()" :disabled="!isAuthorized">Roll</md-button>
+                        <md-button @click.native="clearRolls()" :disabled="!isAuthorized">Clear</md-button>
+                    </md-card-actions>
+                </md-card>
+            </md-layout>
+            <md-layout md-flex-small="100" style="min-width: 50%">
+                <md-card style="flex: 1">
+                    <md-toolbar class="md-dense">
+                        <h2 class="md-title">Clichés</h2>
+                    </md-toolbar>
+                    <md-card-content style="flex: 1">
+                        <md-list class="md-double-line">
+                            <cliche-item  v-for="(cliche, index) in cliches" :cliche="cliche"
+                                          @click.native="rollCliche(cliche)"
+                                          @deleted="onDeleteCliche(index)" :disabled="!isAuthorized"></cliche-item>
+                        </md-list>
+                    </md-card-content>
+                    <md-card-actions>
+                        <md-button @click.native="openNewCliche()" :disabled="!isAuthorized">Add Cliche</md-button>
+                    </md-card-actions>
+                </md-card>
+            </md-layout>
+            <md-layout md-flex-small="100" style="min-width: 50%">
+                <md-card style="flex: 1">
+                    <md-toolbar class="md-dense">
+                        <h2 class="md-title">Hooks</h2>
+                    </md-toolbar>
+                    <md-card-content style="flex: 1">
+                        <md-list>
+                            <hook-item  v-for="(hook, index) in hooks" :hook="hook"
+                                        @deleted="onDeleteHook(index)" :disabled="!isAuthorized"></hook-item>
+                        </md-list>
+                    </md-card-content>
+                    <md-card-actions>
+                        <md-button @click.native="openNewHook()" :disabled="!isAuthorized">Add Hook</md-button>
+                    </md-card-actions>
+                </md-card>
+            </md-layout>
+        </md-layout>
 
         <!-- Dialogs -->
 
@@ -202,9 +199,10 @@
 
     // Managers
     import authMan from '../../../client/api/managers/auth';
+    import charMan from '../../../client/api/managers/character';
+    import sysCharMan from '../../../client/api/managers/sysCharacter';
 
     // Components
-    import CharComponent from '../../../client/components/character.vue';
     import NotesComponent from '../../../client/components/notes/notes.vue';
     import PoolComponent from '../../../client/components/pool.vue';
     import PortraitComponent from '../../../client/components/portrait.vue';
@@ -215,20 +213,15 @@
 
     export default {
         components: {
-            character: CharComponent,
             pool: PoolComponent,
             portrait: PortraitComponent,
             clicheItem: ClicheComponent,
             hookItem: HookComponent
         },
-        props: {
-            character: {
-                type: Object,
-                required: true
-            }
-        },
         subscriptions: {
-            account: authMan.account$
+            account: authMan.account$,
+            baseChar: charMan.selected$,
+            character: sysCharMan.selected$
         },
         data()
         {
@@ -238,8 +231,8 @@
                 dice: undefined,
                 rollName: undefined,
                 rolls: [],
-                advPoints: this.character.advancementPoints,
-                ffDice: this.character.ffDice,
+                advPoints: 0,
+                ffDice: 0,
                 newCliche: {
                     value: undefined,
                     current: undefined,
@@ -252,7 +245,7 @@
             };
         },
         computed: {
-            isAuthorized(){ return _.get(this.account, 'email', 'nope!') === this.character.owner; },
+            isAuthorized(){ return _.get(this.account, 'email', 'nope!') === this.baseChar.owner; },
             cliches()
             {
                 return _.sortBy(this.character.cliches, 'value').reverse();
@@ -352,20 +345,30 @@
         },
         mounted()
         {
-            // PreSave hook
-            this.character.preSave = function()
+            this.$watch('character', () =>
             {
-                // Data validation
-                if(!_.isFinite(this.$system.advancementPoints))
+                if(this.character)
                 {
-                    this.$system.advancementPoints = 0;
-                } // end if
+                    this.advPoints = this.character.advancementPoints;
+                    this.ffDice = this.character.ffDice;
 
-                if(!_.isFinite(this.$system.ffDice))
-                {
-                    this.$system.ffDice = 0;
+                    // PreSave hook
+                    this.character.preSave = function()
+                    {
+                        // Data validation
+                        if(!_.isFinite(this.$system.advancementPoints))
+                        {
+                            this.$system.advancementPoints = 0;
+                        } // end if
+
+                        if(!_.isFinite(this.$system.ffDice))
+                        {
+                            this.$system.ffDice = 0;
+                        } // end if
+                    }; // end preSave
+
                 } // end if
-            } // end preSave
+            });
         }
     }
 </script>
