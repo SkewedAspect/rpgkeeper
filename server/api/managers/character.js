c42347ada6b52b50d447aca84b8232f070e48ddb
Christopher S. Case <chris.case@g33xnexus.com>
2019-01-01 22:57:41 -0600

    Added character manager and rewrote rest endpoint, with unified api.
    Note: This breaks the UI. The separate system character doesn't exist,
    rather it's a JSON field. (Validation for that will come later.) Several
    properties have changed their names; this will be the start of the new
    way of referencing objects / using the API.

---
+++ server/api/managers/character.js
@@ -0,0 +1,83 @@
+//----------------------------------------------------------------------------------------------------------------------
+// CharacterManager
+//----------------------------------------------------------------------------------------------------------------------
+
+const _ = require('lodash');
+
+// Managers
+const notesMan = require('./notes');
+
+// Resource Access
+const characterRA = require('../resource-access/character');
+
+// Logger
+const logger = require('trivial-logging').loggerFor(module);
+
+//----------------------------------------------------------------------------------------------------------------------
+
+class CharacterManager
+{
+    async getCharacters(filters, includeDetails = true)
+    {
+        // Map `id` to `hash_id`
+        if(filters.id)
+        {
+            filters.hash_id = filters.id;
+        } // end if
+
+        // Limit filters to only those properties we want you to be able to search by.
+        filters = _.pick(filters, 'system', 'description', 'portrait', 'thumbnail', 'color', 'biography', 'account_id');
+
+        // Prefix filters with the right table.
+        filters = _.transform(filters, (result, value, key) =>
+        {
+            result[`char.${ key }`] = value;
+            return result;
+        }, {});
+
+        return await characterRA.getCharacters(filters, includeDetails);
+    } // end getCharacters
+
+    async getCharacter(id, includeDetails = true)
+    {
+        return await characterRA.getCharacter(id, includeDetails);
+    } // getCharacterByHash
+
+    async createCharacter(character)
+    {
+        // We have to create a note for this character.
+        const { note_id } = await notesMan.createNote();
+        character.note_id = note_id;
+
+        return await this.getCharacter(await characterRA.addCharacter(character));
+    } // end createCharacter
+
+    async updateCharacter(character)
+    {
+        const characterID = await characterRA.updateCharacter(character);
+        return await characterRA.getCharacter(characterID);
+    } // end updateCharacter
+
+    async deleteCharacter(characterID)
+    {
+        // First, retrieve the character
+        const char = await this.getCharacter(characterID);
+
+        // Next we must delete the character before any other cleanup.
+        const deleted = await characterRA.deleteCharacter(characterID);
+
+        if(deleted > 0)
+        {
+            // Then, we have to delete the notes
+            await notesMan.deleteNote(char.note_id);
+        } // end if
+
+        return deleted;
+    } // end deleteCharacter
+} // end CharacterManager
+
+//----------------------------------------------------------------------------------------------------------------------
+
+module.exports = new CharacterManager();
+
+//----------------------------------------------------------------------------------------------------------------------
