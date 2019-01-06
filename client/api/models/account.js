276436bdf2ec64dfcb86c55734bca2c6d48596fd
Christopher S. Case <chris.case@g33xnexus.com>
2019-01-06 02:53:17 -0600

    moved post and account away from terrible old models.

--- client/api/models/account.js
+++ client/api/models/account.js
@@ -1,27 +1,58 @@
 //----------------------------------------------------------------------------------------------------------------------
 // AccountModel
-//
-// @module
 //----------------------------------------------------------------------------------------------------------------------
 
-import BaseModel from './base';
-
-//----------------------------------------------------------------------------------------------------------------------
-
-class AccountModel extends BaseModel
+class AccountModel
 {
+    constructor(def)
+    {
+        // Set our properties
+        this.$state = Object.assign({
+            id: undefined,
+            email: undefined,
+            name: undefined,
+            avatar: undefined,
+            permissions: [],
+            groups: [],
+            settings: {},
+            created: Date.now()
+        }, def);
+    } // end constructor
+
     //------------------------------------------------------------------------------------------------------------------
     // Properties
     //------------------------------------------------------------------------------------------------------------------
 
-    get id(){ return this.$state.hash_id; }
-
+    get id(){ return this.$state.id; }
+    get email(){ return this.$state.email; }
+    get name(){ return this.$state.name; }
     get displayName(){ return this.$state.name || this.$state.email; }
+    get avatar(){ return this.$state.avatar; }
     get avatarUrl()
     {
         const id = this.id.replace(/-/g, '');
         return this.$state.avatar || `https://identicons.github.com/${ id }.png`;
     }
+    get groups(){ return this.$state.groups; }
+
+    get permissions(){ return this.$state.permissions; }
+    set permissions(val){ this.$state.permissions = val; }
+    get settings(){ return this.$state.permissions; }
+    set settings(val){ this.$state.settings = val; }
+
+    //------------------------------------------------------------------------------------------------------------------
+    // Model API
+    //------------------------------------------------------------------------------------------------------------------
+
+    update(def)
+    {
+        this.$state = Object.assign(this.$state, def);
+    } // end update
+
+    toJSON()
+    {
+        return Object.assign({}, this.$state);
+    } // end toJSON
 } // end AccountModel
 
 //----------------------------------------------------------------------------------------------------------------------
