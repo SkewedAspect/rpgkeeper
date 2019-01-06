d380d47be8cf79df3c9f405c4832e5e56fab64fa
Christopher S. Case <chris.case@g33xnexus.com>
2019-01-06 02:29:41 -0600

    added validations for FATE.

--- systems/fate/validations.js
+++ systems/fate/validations.js
@@ -5,9 +5,78 @@
 module.exports = {
     character: {
         type: 'object',
-        required: [],
+        required: [ 'fatePoints', 'aspects', 'skills', 'extras', 'stunts', 'physicalStress', 'mentalStress' ],
         properties: {
-        }
+            fatePoints: {
+                type: 'object',
+                required: [ 'refresh', 'current' ],
+                properties: {
+                    refresh: { type: 'integer', min: 0 },
+                    current: { type: 'integer', min: 0 }
+                },
+                additionalProperties: false
+            },
+            aspects: {
+                type: 'array',
+                items: {
+                    type: 'object',
+                    required: [ 'type', 'detail', 'healing', 'value' ],
+                    properties: {
+                        type: { type: 'string', enum: [ 'aspect', 'high concept', 'trouble', 'consequence' ] },
+                        detail: { type: 'string', minLength: 1 },
+                        healing: { type: 'boolean' },
+                        value: { type: 'integer', min: 0 }
+                    },
+                    additionalProperties: false
+                }
+            },
+            skills: {
+                type: 'array',
+                items: {
+                    type: 'object',
+                    required: [ 'name', 'rank' ],
+                    properties: {
+                        name: { type: 'string' },
+                        rank: { type: 'integer', min: 0 }
+                    },
+                    additionalProperties: false
+                }
+            },
+            extras: { type: 'string' },
+            stunts: {
+                type: 'array',
+                items: {
+                    type: 'object',
+                    required: [ 'title', 'description' ],
+                    properties: {
+                        title: { type: 'string', minLength: 1 },
+                        description: { type: 'string', minLength: 1 }
+                    },
+                    additionalProperties: false
+                }
+            },
+            physicalStress: {
+                type: 'array',
+                items: [
+                    { type: 'boolean' },
+                    { type: 'boolean' },
+                    { type: 'boolean' },
+                    { type: 'boolean' }
+                ],
+                additionalItems: false
+            },
+            mentalStress: {
+                type: 'array',
+                items: [
+                    { type: 'boolean' },
+                    { type: 'boolean' },
+                    { type: 'boolean' },
+                    { type: 'boolean' }
+                ],
+                additionalItems: false
+            }
+        },
+        additionalProperties: false
     }
 }; // end exports
 
