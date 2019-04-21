f2135f2e8aa3b68469999e1eaa5f9116a13b4348
Christopher Case <chris.case@g33xnexus.com>
2019-04-21 18:47:26 -0500

    improved model updating.

--- client/api/models/character.js
+++ client/api/models/character.js
@@ -15,13 +15,10 @@ class CharacterModel
     constructor(def, sysDefaults = {})
     {
         // Save off the defaults for the system specific portion.
-        this._sysDefaults = sysDefaults;
+        this._sysDefaults = _.cloneDeep(sysDefaults);
 
         // Set our properties
-        this.$state = _.merge({}, this.$defaults, _.cloneDeep(def));
-
-        // Store our reference model to revert back to
-        this.$refState = _.merge({}, this.$defaults, _.cloneDeep(def));
+        this.update(def);
 
         // Mark the ref state as non-configurable, so vue ignores it.
         markNonConfigurable(this, '$refState');
@@ -83,8 +80,9 @@ class CharacterModel
 
     update(def)
     {
-        this.$state = _.merge({}, this.$defaults, _.cloneDeep(def));
-        this.$refState = _.merge({}, this.$defaults, _.cloneDeep(def));
+        const defaults = _.cloneDeep(this.$defaults);
+        this.$state = _.merge({}, defaults, _.cloneDeep(def));
+        this.$refState = _.merge({}, defaults, _.cloneDeep(def));
     } // end update
 
     toJSON()
