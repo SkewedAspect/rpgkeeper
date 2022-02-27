fa2aa6d0c7b7d7e25e9190a60466fea3086eb794
Christopher S. Case <chris.case@g33xnexus.com>
2022-02-26 20:40:42 -0600

    Updated  vite config
    Updated vite config from another project. Will still need editing, though.

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,66 @@
 //----------------------------------------------------------------------------------------------------------------------
-// vite.config.js
+// Vite Config
 //----------------------------------------------------------------------------------------------------------------------
 
+import path from 'path';
+import { defineConfig } from 'vite';
+
+// Vite Plugins
 import { createVuePlugin } from 'vite-plugin-vue2';
 
 //----------------------------------------------------------------------------------------------------------------------
 
-export default {
+export default defineConfig({
+    root: 'client',
+    publicDir: 'static',
     plugins: [
         createVuePlugin()
-    ]
-};
+    ],
+    server: {
+        port: 8089,
+        proxy: {
+            '/api': 'http://localhost:8090',
+            '/config': 'http://localhost:8090',
+            '/session': 'http://localhost:8090',
+            '/version': 'http://localhost:8090',
+            '/socket.io': {
+                target: 'http://localhost:8090',
+                ws: true
+            }
+        },
+        https: false,
+        open: false
+    },
+    resolve: {
+        alias: [
+            {
+                find: /~(.+)/,
+                replacement: path.join(process.cwd(), 'node_modules/$1')
+            },
+            {
+                find: 'bootstrap-vue$',
+                replacement: 'bootstrap-vue/src/index.js'
+            },
+            {
+                find: 'vue',
+                replacement: 'vue/dist/vue.esm.js'
+            }
+        ]
+    },
+    build: {
+        outDir: '../../dist/client',
+        emptyOutDir: true,
+        cssCodeSplit: true,
+        chunkSizeWarningLimit: 650,
+        rollupOptions: {
+            output: {
+                manualChunks: {
+                    bootstrap: [ 'popper.js', 'jquery', 'bootstrap' ],
+                    bootstrapVue: [ 'bootstrap-vue' ],
+                }
+            }
+        }
+    }
+});
 
 //----------------------------------------------------------------------------------------------------------------------
